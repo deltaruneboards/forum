820b2cf79351d69172f0f814e0cba4c23ed82a6c
jamirror <66569648+justamirror@users.noreply.github.com>
2026-09-08 21:30:33 -0400

    comment time

--- Themes/default/Display.template.php
+++ Themes/default/Display.template.php
@@ -639,7 +639,7 @@ function template_single_post($message)
 								<li class="poster_ip">
 									<a href="', $scripturl, '?action=', !empty($message['member']['is_guest']) ? 'trackip' : 'profile;area=tracking;sa=ip;u=' . $message['member']['id'], ';searchip=', $message['member']['ip'], '" data-hover="', $message['member']['ip'], '" class="show_on_hover"><span>', $txt['show_ip'], '</span></a> <a href="', $scripturl, '?action=helpadmin;help=see_admin_ip" onclick="return reqOverlayDiv(this.href);" class="help">(?)</a>
 								</li>';
-
+	/* DUMB change: no reason to show this to regular users
 	// Or, should we show it because this is you?
 	elseif ($message['can_see_ip'])
 		echo '
@@ -658,7 +658,7 @@ function template_single_post($message)
 	else
 		echo '
 								<li class="poster_ip">', $txt['logged'], '</li>';
-
+	*/
 	// Are we showing the warning status?
 	// Don't show these things for guests.
 	if (!$message['member']['is_guest'] && $message['member']['can_see_warning'])
