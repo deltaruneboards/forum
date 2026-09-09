4d5323173b808d5823e3ee2ec105ea7975ec453a
jamirror <66569648+justamirror@users.noreply.github.com>
2026-09-08 21:29:46 -0400

    Revert "gut the ip logged message and remove the ability for users to see their own ip. its scary"
    This reverts commit 6d84aed13e1d3b734171362553195210abf34780.

--- Themes/default/Display.template.php
+++ Themes/default/Display.template.php
@@ -639,7 +639,26 @@ function template_single_post($message)
 								<li class="poster_ip">
 									<a href="', $scripturl, '?action=', !empty($message['member']['is_guest']) ? 'trackip' : 'profile;area=tracking;sa=ip;u=' . $message['member']['id'], ';searchip=', $message['member']['ip'], '" data-hover="', $message['member']['ip'], '" class="show_on_hover"><span>', $txt['show_ip'], '</span></a> <a href="', $scripturl, '?action=helpadmin;help=see_admin_ip" onclick="return reqOverlayDiv(this.href);" class="help">(?)</a>
 								</li>';
-								
+
+	// Or, should we show it because this is you?
+	elseif ($message['can_see_ip'])
+		echo '
+								<li class="poster_ip">
+									<a href="', $scripturl, '?action=helpadmin;help=see_member_ip" onclick="return reqOverlayDiv(this.href);" class="help show_on_hover" data-hover="', $message['member']['ip'], '"><span>', $txt['show_ip'], '</span></a>
+								</li>';
+
+	// Okay, are you at least logged in? Then we can show something about why IPs are logged...
+	elseif (!$context['user']['is_guest'])
+		echo '
+								<li class="poster_ip">
+									<a href="', $scripturl, '?action=helpadmin;help=see_member_ip" onclick="return reqOverlayDiv(this.href);" class="help">', $txt['logged'], '</a>
+								</li>';
+
+	// Otherwise, you see NOTHING!
+	else
+		echo '
+								<li class="poster_ip">', $txt['logged'], '</li>';
+
 	// Are we showing the warning status?
 	// Don't show these things for guests.
 	if (!$message['member']['is_guest'] && $message['member']['can_see_warning'])
